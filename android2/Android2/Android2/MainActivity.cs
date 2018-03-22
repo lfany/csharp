618d36e8d9955ba32d9622160a26489769ca9a28
hello <hello@ifnot.cc>
2018-03-22 14:03:05 +0800

    hello world

--- android2/Android2/Android2/MainActivity.cs
+++ android2/Android2/Android2/MainActivity.cs
@@ -1,18 +1,25 @@
 ﻿using Android.App;
-using Android.Widget;
 using Android.OS;
+using Android.Util;
+using System;
 
 namespace Android2
 {
     [Activity(Label = "Android2", MainLauncher = true, Icon = "@drawable/icon")]
     public class MainActivity : Activity
     {
+        private String TAG = typeof(MainActivity).ToString();
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             // Set our view from the "main" layout resource
             // SetContentView (Resource.Layout.Main);
+
+            Console.WriteLine("Hello world");
+            Console.Write("~~~~\n");
+            Log.Verbose(TAG, "log test");
         }
     }
 }
